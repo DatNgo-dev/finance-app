4704cb91bac000eb533e6fd547d5c1903f0e9e18
Dat Ngo <119279710+DatNgo-dev@users.noreply.github.com>
2023-08-24 22:40:14 +0800

    Removed Next-Auth for Supabase Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { signIn } from "next-auth/react";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={() => signIn}>Sign In</button>
+      <button>Sign In</button>
     </main>
   );
 }
