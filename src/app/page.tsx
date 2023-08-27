a13c93bee70f407f2303ef7fa71cf86afceff196
Dat Ngo <119279710+DatNgo-dev@users.noreply.github.com>
2023-08-27 20:05:34 +0800

    Implemented supabase authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+import Login from "./components/client/auth/Login";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button>Sign In</button>
+      <Login></Login>
     </main>
   );
 }
